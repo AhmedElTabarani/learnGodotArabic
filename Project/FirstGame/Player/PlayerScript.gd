4e4a35c663fbb406efe53cfbb08ea5376d9eda9a
AhmedElTabarani <nonomomo556677@gmail.com>
2021-02-21 05:02:50 +0200

    اضافة نص الحركة البرمجي

--- Project/FirstGame/Player/PlayerScript.gd
+++ Project/FirstGame/Player/PlayerScript.gd
@@ -1,16 +1,20 @@
 extends KinematicBody2D
 
+var speed = 500
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _physics_process(delta):
+	# Move Right +x
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	# Move Left -x
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+	
+	# Move Up -y
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed * delta
+	
+	# Move Down +y
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed * delta
