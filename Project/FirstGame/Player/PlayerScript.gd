22c3678a17815c3ef54d9b5af6e4aa9050d61194
أحمد الطبراني <eltabaraniahmed@gmail.com>
2021-04-15 00:30:57 +0200

    اضافة التصادم

--- Project/FirstGame/Player/PlayerScript.gd
+++ Project/FirstGame/Player/PlayerScript.gd
@@ -4,30 +4,31 @@ var speed = 100
 var direction : Vector2
 
 func _physics_process(delta):
-	# define direction of movement
-	direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	
-	# make a vector transform into the unit vector
-	# i.e. its length equals 1
-	direction = direction.normalized()
-	
-	# Movement
-	if direction:
-		position += direction * speed * delta
-		
-	else: # stop animation movement
-		$AnimatedSprite.stop()
-		$AnimatedSprite.frame = 0
-	
-	# animation movement for right/left
-	if direction.x == 1:
-		$AnimatedSprite.play("right")
-	elif direction.x == -1:
-		$AnimatedSprite.play("left")
-	
-	# animation movement for down/up
-	if direction.y == 1:
-		$AnimatedSprite.play("down")
-	elif direction.y == -1:
-		$AnimatedSprite.play("up")
+    # define direction of movement
+    direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+    direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+    
+    # make a vector transform into the unit vector
+    # i.e. its length equals 1
+    direction = direction.normalized()
+    
+    # Movement
+    if direction:
+        # handle the movement of the player and the collision with objects
+        move_and_slide(direction * speed)
+        
+    else: # stop animation movement
+        $AnimatedSprite.stop()
+        $AnimatedSprite.frame = 0
+    
+    # animation movement for right/left
+    if direction.x == 1:
+        $AnimatedSprite.play("right")
+    elif direction.x == -1:
+        $AnimatedSprite.play("left")
+    
+    # animation movement for down/up
+    if direction.y == 1:
+        $AnimatedSprite.play("down")
+    elif direction.y == -1:
+        $AnimatedSprite.play("up")
